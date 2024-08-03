6ddc78a889aa4b422ec305e51fdd43a54111222a
Utpal Mishra <utpalmishra21official@gmail.com>
2024-08-03 18:21:39 +0100

    Added Tanzania Geolocation Data

--- app/home.py
+++ app/home.py
@@ -1,7 +1,7 @@
 # LIBRARIES
 
 import streamlit as st
-import time
+import pandas as pd
 
 print('\nLibraries Imported\n')
 
@@ -31,6 +31,15 @@ def app():
         st.subheader("GOAL: \n**SMART: Specific, Measurable, Attainable, Relevant, and Time-bound**\n\n**Specific:**\nDevelop a robust AI-powered early warning system to predict extreme weather conditions, specifically floods, heavy rains, and heat waves, in Tanzania. This system will leverage data from the Tanzania Meteorological Authority (TMA) and historical weather data to provide accurate and timely alerts eventually via SMS to residents, enhancing their preparedness and response to weather-related disasters.\n\n**Measureable**\ni. Achieve a prediction accuracy of at least 80% for floods, heavy rains, and heat waves within the first 8 weeks of model development\nii. Develop and deploy a user-friendly dashboard that visualizes weather predictions and system performance (a dashboard example for inspiration can be found here).\n\n**Achievable**\nUtilize existing machine learning frameworks and pre-trained models for weather prediction. Collaborate with TMA for access to historical weather data and leverage the expertise of Omdena's global network of AI specialists and data scientists to ensure the project's success.\n\n**Relevant**\nThis project addresses the critical need for timely and accurate weather information in Tanzania, which is prone to extreme weather events due to climate change. By providing early warnings, the system will help save lives, reduce property damage, and improve disaster responsiveness, aligning with the goals of local government authorities and NGOs working on climate resilience.\n\n**Time-Bound**\nComplete the development and initial deployment of the Kesho AI system within 8 +2 weeks\n\n")
         
         st.divider() 
+        
+        data = pd.read_csv('locations.csv')
+        data.drop(data.columns[[0]], axis=1, inplace=True)
+        data['Region'] = data['Region'].apply(lambda x: x.title())
+        
+        data = data.groupby(['Region', 'Council', 'Ward']).mean()
+        data['Population'] = data['Population'].astype(int)
+        
+        st.dataframe(data)
         
     with tab2:
         
